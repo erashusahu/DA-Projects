9289dd70da6a889db443085d22905aafcf744b80
Ashutosh <sahuashutosh1408@gmail.com>
2025-12-26 01:51:55 +0530

    changes done

--- Project2/function.py
+++ Project2/function.py
@@ -64,4 +64,32 @@ def get_basic_info(cursor):
     return results
 
 
+def add_new_manual_id(cursor, db, p_name , p_category , p_price , p_stock , p_reorder, p_supplier):
+    proc_call= "call AddNewProductManualID(%s, %s, %s ,%s ,%s, %s)"
+    params= (p_name , p_category , p_price , p_stock , p_reorder, p_supplier)
+    cursor.execute(proc_call, params)
+    db.commit()
+    
+
+def get_categories(cursor):
+    cursor.execute("select Distinct category  from products  order by category  asc")
+    rows= cursor.fetchall()
+    return [row["category"] for row in rows]
+
+
+def get_suppliers(cursor):
+    cursor.execute("select supplier_id , supplier_name from suppliers order by  supplier_name asc")
+    return cursor.fetchall()
+
+
+def get_product_histroy(cursor,product_id):
+   query="select * from product_inventory_histroy where product_id=%s order by record_date Desc"
+   cursor.execute(query,(product_id,))
+   return cursor.fetchall()
+
+
+def get_all_products(cursor):
+    cursor.execute("select product_id, product_name from products order by product_name asc")
+    return cursor.fetchall()
+
 
